eb27be9a436ecd0878b8a4b8ef78a18c2a21af21
Natalia Velez <nvelez@stanford.edu>
2018-11-08 14:44:49 -0800

    cleaned up preprocessing scripts

--- 2_preprocessing/2_3_mriqc_subject.sh
+++ 2_preprocessing/2_3_mriqc_subject.sh
@@ -1,3 +1,3 @@
 for i in {3..8}; do
-	sbatch 1_3_mriqc_subject.sbatch $(printf %02d $i)
+	sbatch 2_3_mriqc_subject.sbatch $(printf %02d $i)
 done
--- 2_preprocessing/2_5_smooth.sh
+++ 2_preprocessing/2_5_smooth.sh
@@ -1,5 +1,5 @@
 for i in {5..23}; do
 	SUBJECT=$(printf sub-%02d $i)
 	echo "Smoothing data for: " $SUBJECT
-	sbatch smoothing_postfmriprep.sbatch SwiSt $SUBJECT
+	sbatch 2_5_smooth.sbatch OPUS $SUBJECT
 done
