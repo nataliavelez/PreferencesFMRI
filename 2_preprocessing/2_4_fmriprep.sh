97c7400de20bba2934d6ec9efab61e6710f46426
Natalia Velez <nvelez@stanford.edu>
2018-11-14 10:52:49 -0800

    fixed smoothing and added RSA

--- 2_preprocessing/2_4_fmriprep.sh
+++ 2_preprocessing/2_4_fmriprep.sh
@@ -1,3 +1,3 @@
 for i in {3..8}; do
-	sbatch 1_4_fmriprep.sbatch $(printf sub-%02d $i)
+	sbatch 2_4_fmriprep.sbatch $(printf sub-%02d $i)
 done
--- 2_preprocessing/2_5_smooth.sh
+++ 2_preprocessing/2_5_smooth.sh
@@ -1,4 +1,4 @@
-for i in {5..23}; do
+for i in {3..8}; do
 	SUBJECT=$(printf sub-%02d $i)
 	echo "Smoothing data for: " $SUBJECT
 	sbatch 2_5_smooth.sbatch OPUS $SUBJECT
